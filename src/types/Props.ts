947c8d23f3a6f5f0a74625f8c5e2a14c2db0c0a8
Ignacio Essus <i.essus01@ufromail.cl>
2025-11-21 20:21:48 -0300

    Subir Props para reserva

---
+++ src/types/Props.ts
@@ -0,0 +1,6 @@
+export interface Props {
+    roomId?: string;              // p.ej. "1"
+    startISO?: string;            // p.ej. "2024-07-23T10:00:00-04:00"
+    endISO?: string;              // p.ej. "2024-07-23T11:00:00-04:00"
+    titleOverride?: string;       // opcional para personalizar el título del evento
+};
